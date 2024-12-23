b754df614b51b694d9c9b20fe86272c47b7e6c8a
AnranYus <anranyus@hotmail.com>
2024-12-24 00:21:59 +0800

    Init Pong Android 14

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_PATH)/Pong.mk
+	
+COMMON_LUNCH_CHOICES := \
+	Pong-eng
---
+++ Pong.mk
@@ -0,0 +1,9 @@
+$(call inherit-product,device/nothing/Pong/device.mk)
+
+TARGET_LINUX_KERNEL_VERSION := 5.10
+
+PRODUCT_NAME := aosp_Pong
+PRODUCT_DEVICE := Pong
+PRODUCT_MODEL := AOSP on Pong
+PRODUCT_BRAND := Android
+PRODUCT_MANUFACTURER := Nothing
